5871a0b05d117ac21a5043c556fbd8b275d3907c
BurbanoJaime <burbanojaime98@gmail.com>
2017-11-24 14:51:20 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,101 @@
+//Transporte
+$(".menu-transporte").mouseover(function(){
+    $(".menu-transporte-contenido")
+        .css({
+        'display':'inherit'  
+    })
+});
+
+
+$(".menu-transporte").mouseout(function(){
+
+    $(".menu-transporte-contenido").css({
+        'display':'none'
+
+    })
+});
+
+// Scouting
+$(".menu-scouting").mouseover(function(){
+    $(".menu-scouting-contenido")
+        .css({
+        'display':'inherit'  
+    })
+});
+
+
+$(".menu-scouting").mouseout(function(){
+
+    $(".menu-scouting-contenido").css({
+        'display':'none'
+
+    })
+});
+
+// casting
+$(".menu-casting").mouseover(function(){
+    $(".menu-casting-contenido")
+        .css({
+        'display':'inherit'  
+    })
+});
+
+
+$(".menu-casting").mouseout(function(){
+
+    $(".menu-casting-contenido").css({
+        'display':'none'
+
+    })
+});
+
+// equipo
+$(".menu-equipos").mouseover(function(){
+    $(".menu-equipos-contenido")
+        .css({
+        'display':'inherit'  
+    })
+});
+
+
+$(".menu-equipos").mouseout(function(){
+
+    $(".menu-equipos-contenido").css({
+        'display':'none'
+
+    })
+});
+
+// decoración
+$(".menu-decoracion").mouseover(function(){
+    $(".menu-decoracion-contenido")
+        .css({
+        'display':'inherit'  
+    })
+});
+
+
+$(".menu-decoracion").mouseout(function(){
+
+    $(".menu-decoracion-contenido").css({
+        'display':'none'
+
+    })
+});
+
+// otros
+$(".menu-otros").mouseover(function(){
+    $(".menu-otros-contenido")
+        .css({
+        'display':'inherit'  
+    })
+});
+
+
+$(".menu-otros").mouseout(function(){
+
+    $(".menu-otros-contenido").css({
+        'display':'none'
+
+    })
+});
